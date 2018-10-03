77790a7dbf495d1dea40cbd684b45bdce021b815
Waralee <waralee.ming@gmail.com>
2018-10-03 20:29:18 +0700

    Add files via upload

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -4,6 +4,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -33,8 +34,10 @@ public class TFIDFSearcher extends Searcher
 					countDoc++;
 				}
 			}
-			double idf = Math.log(1+(super.documents.size()/countDoc));
+//			System.out.println(q+"   =   "+countDoc);
+			double idf = Math.log(1+(super.documents.size()/(double)countDoc));
 			idfscore.put(q, idf);
+			
 		}
 		
 //		for(String key : idfscore.keySet()){
@@ -50,20 +53,58 @@ public class TFIDFSearcher extends Searcher
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
 		
+		List<SearchResult> result = new ArrayList<SearchResult>();
+		Map<String, Double> docVector = new HashMap<>();
+		Map<String, Double> qVector = new HashMap<>();		
+		
 		//Tokenize query
 		List<String> query = super.tokenize(queryString);
 		List<String> queryList = new ArrayList<>();
 		for(String str: query){
 			if(!queryList.contains(str)){
 				queryList.add(str);
+				vocab.add(str);
 			}
 		}
+		
+		for(String str:queryList){
+			int count = 0;
+			for(Document d: super.documents){
+				if(d.getTokens().contains(str)){
+					count++;
+				}
+			}
+			double idf = Math.log(1+(super.documents.size()/(double)count));
+			idfscore.put(str, idf);
+		}
+		
+		for(String q:vocab){
+			int count = 0;
+			double tf = 0;
+			double weight = 0;
+			for(String str:queryList){
+				if(str.equals(q)){
+					count++;
+				}
+			}
+			if(count == 0){
+				tf = 0;
+			}
+			else{
+				tf = 1 + Math.log(count);
+			}
+			weight = tf*idfscore.get(q);
+			qVector.put(q, weight);
+		}
+		
+		for(String i : qVector.keySet()){
+			System.out.println(i+" "+qVector.get(i));
+		}
 				
-		List<SearchResult> result = new ArrayList<SearchResult>();
-
+		
 		for(Document d : super.documents){
-			double score = 0;
-			for(String q : d.getTokens()){
+			double weight = 0;
+			for(String q : vocab){
 				int count = 0;
 				for(String s: d.getTokens()){
 					if(s.equals(q)){
@@ -78,18 +119,26 @@ public class TFIDFSearcher extends Searcher
 				else{
 					tfscore = 1 + Math.log(count);
 				}
-				score = tfscore*idfscore.get(q);
-				System.out.println("Doc"+d.getId()+" Query "+q+"TF score : "+tfscore+" idfscore: "+idfscore.get(q) +" score : "+score);
+				weight = tfscore*idfscore.get(q);
+				docVector.put(q, weight);
+				System.out.println("Doc"+d.getId()+" Query "+q+"TF score : "+tfscore+" idfscore: "+idfscore.get(q) +" weight : "+weight);
+				
 			}
-			
-			SearchResult resultScore = new SearchResult(d, score);
-			result.add(resultScore);
-			break;
+			double score = 0;
+			double sized = 0;
+			double sizeq = 0;
+			for(String term:vocab){
+				score += docVector.get(term)*qVector.get(term);
+				sized += docVector.get(term)*docVector.get(term);
+				sizeq += qVector.get(term)*qVector.get(term);
+			}
+			score = score/(Math.sqrt(sized)+Math.sqrt(sizeq));	
+			SearchResult docResult = new SearchResult(d, score);
+			result.add(docResult);
 		}
 		
+		
 		Collections.sort(result);
-		
-		
 		
 		return result.subList(0, k);
 		/***********************************************/
